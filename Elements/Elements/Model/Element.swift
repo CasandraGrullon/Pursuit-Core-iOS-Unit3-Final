2c8dadf9092497d03ce91a18bf47b554803dfb1c
Casandra <casandragrullon@pursuit.org>
2019-12-19 10:39:03 -0500

    finished ui elements

---
+++ Elements/Elements/Model/Element.swift
@@ -0,0 +1,11 @@
+//
+//  Element.swift
+//  Elements
+//
+//  Created by casandra grullon on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct Element: Codable
