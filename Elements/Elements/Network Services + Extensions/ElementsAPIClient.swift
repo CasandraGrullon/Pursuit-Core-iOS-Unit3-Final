7f3ae6389320d062740290f03993f00cb7872834
Casandra <casandragrullon@pursuit.org>
2019-12-19 11:17:33 -0500

    finished get faves and post faves in api client

--- Elements/Elements/Network Services + Extensions/ElementsAPIClient.swift
+++ Elements/Elements/Network Services + Extensions/ElementsAPIClient.swift
@@ -30,8 +30,61 @@ struct ElementsAPIClient {
                     completion(.success(elements))
                 } catch {
                     completion(.failure(.decodingError(error)))
+                }
             }
         }
     }
-}
+    static func getFaves(completion: @escaping (Result<[Element], AppError>) -> () ) {
+        
+        let endpointURL = "http://5c1d79abbc26950013fbcaa9.mockapi.io/api/v1/favorites"
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do{
+                    let favorites = try JSONDecoder().decode([Element].self, from: data)
+                    completion(.success(favorites))
+                }catch{
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+    static func postFaves(favorite: Element, completion: @escaping (Result<Bool, AppError>) -> () ){
+        
+        let endpointURL = "http://5c1d79abbc26950013fbcaa9.mockapi.io/api/v1/favorites"
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        
+        do{
+            let data = try JSONEncoder().encode(favorite)
+            var request = URLRequest(url: url)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            request.httpBody = data
+            NetworkHelper.shared.performDataTask(with: request) { (result) in
+                switch result{
+                case .failure(let appError):
+                    completion(.failure(.networkClientError(appError)))
+                case .success:
+                    completion(.success(true))
+                }
+            }
+        } catch {
+            completion(.failure(.encodingError(error)))
+        }
+        
+        
+    }
 }
